1e3699ae99cdf2ad4844557f34a75216ea0af3ea
Francis Eshun <38195639+BrainyWayne@users.noreply.github.com>
2019-03-27 19:40:49 +0000

    Create Master Sorter.bat

---
+++ Master Sorter.bat
@@ -0,0 +1,53 @@
+
+@echo off
+color 0a
+cls
+
+title Moving Movies
+echo Moving Movies-----------------
+move *.mp4 F:\
+move *.mkv F:\
+move *.avi F:\
+echo Done Moving Movies------------
+title Done Moving Movies
+
+title Moving Softwares
+echo Moving softwares -----------------
+move *.exe G:\Softwares
+echo Done Moving softwares ------------
+title Done Moving softwares 
+
+title Moving Isos
+echo Moving Isos-----------------
+move *.iso G:\Isos
+move *.zip G:\Isos
+move *.rar G:\Isos
+move *.jar G:\Isos
+echo Done Moving Isos------------
+title Done Moving Isos
+
+title Moving Pictures
+echo Moving Pictures-----------------
+move *.jpg G:\Pictures
+move *.jpeg G:\Pictures
+move *.bmp G:\Pictures
+move *.png G:\Pictures
+echo Done Moving Pictures------------
+title Done Moving Pictures
+
+title Moving Docs
+echo Moving Documents ---------------
+move *.txt G:\Documents
+move *.doc G:\Documents
+move *.docx G:\Documents
+move *.pdf G:\Documents
+move *.htm G:\Documents
+move *.ipyb G:\Documents
+move *.ipynb G:\Documents
+move *.html G:\Documents
+move *.ppt G:\Documents
+move *.pptx G:\Documents
+move *.sql "G:\Course Files\2nd yr\Database\Lab works"
+echo Done moving documents -----------
+title Done moving documens 
+
